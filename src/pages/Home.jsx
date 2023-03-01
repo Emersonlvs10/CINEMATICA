f4d62a4c4dd785d83b42dc537de19dd18d02998a
Emerson <emersonlvs10@gmail.com>
2023-03-01 14:49:15 -0300

    Projeto final

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+import "./MoviesGrid.css";
+
+const moviesURl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRateUrl = `${moviesURl}top_rated?${apiKey}`;
+    getTopMovies(topRateUrl);
+  }, []);
+  return (
+    <div className="container">
+      <h2 className="title">Melhores Filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
